07d28852374b4c46a2f3ef1926716e17211f16c2
Serkan <serkankarraa@gmail.com>
2022-03-24 14:57:32 +0300

    Initial Commit

---
+++ DailyNewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyNewsApp
+//
+//  Created by Serkan on 24.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
