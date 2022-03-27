21be655b1d849902505d44703676ddff16bf7b7f
Serkan <serkankarraa@gmail.com>
2022-03-27 10:05:37 +0300

    created favorites scenes

---
+++ DailyNewsApp/Scenes/Favorites/FavoritesCell/FavoritesCell.swift
@@ -0,0 +1,66 @@
+//
+//  FavoritesCell.swift
+//  DailyNewsApp
+//
+//  Created by Serkan on 26.03.2022.
+//
+
+import UIKit
+
+class FavoritesCell: UITableViewCell {
+    
+    static let reuseId = "cell"
+    
+    var favoriteCellImage       = NImageView(frame: .zero)
+    var favoriteCellLabel       = NLabel(fontSize: 20)
+    var favoriteCellDateLabel   = NLabel(fontSize: 15)
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        configure()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    func set(news: TitleItem){
+        favoriteCellLabel.text = news.title
+        favoriteCellImage.downloadImage(from: news.urlToImage ?? "image")
+        favoriteCellDateLabel.text = news.publishedAt
+        configure()
+    }
+    
+    private func configure(){
+        addSubview(favoriteCellImage)
+        addSubview(favoriteCellLabel)
+        addSubview(favoriteCellDateLabel)
+        
+        favoriteCellLabel.translatesAutoresizingMaskIntoConstraints = false
+        favoriteCellImage.translatesAutoresizingMaskIntoConstraints = false
+        favoriteCellDateLabel.translatesAutoresizingMaskIntoConstraints = false
+        favoriteCellDateLabel.textAlignment = .right
+        favoriteCellDateLabel.textColor = .systemGray
+        
+        NSLayoutConstraint.activate([
+            favoriteCellImage.topAnchor.constraint(equalTo: topAnchor, constant: 10),
+            favoriteCellImage.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            favoriteCellImage.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10),
+            favoriteCellImage.widthAnchor.constraint(equalTo: favoriteCellImage.heightAnchor),
+            
+            favoriteCellLabel.topAnchor.constraint(equalTo: topAnchor, constant: 10),
+            favoriteCellLabel.leadingAnchor.constraint(equalTo: favoriteCellImage.trailingAnchor, constant: 10),
+            favoriteCellLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            favoriteCellLabel.heightAnchor.constraint(equalToConstant: 50),
+            
+            favoriteCellDateLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10),
+            favoriteCellDateLabel.leadingAnchor.constraint(equalTo: favoriteCellImage.leadingAnchor, constant: 10),
+            favoriteCellDateLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            favoriteCellDateLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+        
+        
+    }
+
+}
---
+++ DailyNewsApp/Scenes/Favorites/FavoritesVC.swift
@@ -0,0 +1,126 @@
+//
+//  FavoritesVC.swift
+//  DailyNewsApp
+//
+//  Created by Serkan on 24.03.2022.
+//
+
+import UIKit
+import CoreData
+
+class FavoritesVC: UIViewController {
+    
+    private var favorites: [TitleItem] = [TitleItem]()
+    private var favoritess: TitleItem!
+    
+    private let favoritesTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(FavoritesCell.self, forCellReuseIdentifier: FavoritesCell.reuseId)
+        return tableView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        configureNavBar()
+        configureFavoritesTableView()
+        fetchLocalStorageForDownload()
+        Notify()
+    }
+    
+    func configureNavBar(){
+        title = "Favorites"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+    }
+    
+    
+    func Notify(){
+        NotificationCenter.default.addObserver(forName: NSNotification.Name("downloaded"), object: nil, queue: nil) { _ in
+            self.fetchLocalStorageForDownload()
+        }
+    }
+    
+    
+    func configureFavoritesTableView(){
+        view.addSubview(favoritesTableView)
+        favoritesTableView.delegate     = self
+        favoritesTableView.dataSource   = self
+    }
+    
+    
+    private func fetchLocalStorageForDownload(){
+        DataPersistanceManager.shared.fetchingFavoritesFromDataBase { [weak self] result in
+            switch result{
+            case .success(let titles):
+                self?.favorites = titles
+                DispatchQueue.main.async { self?.favoritesTableView.reloadData() }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        favoritesTableView.frame = view.bounds
+    }
+}
+
+
+extension FavoritesVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favorites.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //let cell = favoritesTableView.dequeueReusableCell(withIdentifier: FavoritesCell.reuseId, for: indexPath) as! FavoritesCell
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: FavoritesCell.reuseId, for: indexPath) as? FavoritesCell else {
+            return UITableViewCell()
+        }
+        
+        let favorite = favorites[indexPath.row]
+        
+        cell.set(news: favorite)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        switch editingStyle {
+        case .delete:
+            
+            DataPersistanceManager.shared.deleteTitleWith(model: favorites[indexPath.row]) { result in
+                switch result {
+                case .success():
+                    print("deleted from the database")
+                case . failure(let error):
+                    print(error.localizedDescription)
+                }
+                self.favorites.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+        default:
+            break
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let new = favorites[indexPath.item]
+        let destVC = NewsDetailVC()
+        
+        destVC.titleLabel.text              = new.title
+        destVC.dateLabel.text               = new.publishedAt
+        destVC.descriptionTextView.text     = new.descriptionn
+        destVC.configureLearnMoreButton(name: new.urll ?? "")
+        destVC.imageView.downloadImage(from: new.urlToImage ?? "")
+        
+        let navController = UINavigationController(rootViewController: destVC)
+        present(navController, animated: true)
+    }
+    
+}
